9ac2adea55369d0703e5f99723b18b66780847df
dawncronin <github email address>
2019-12-09 14:45:25 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+
 // Write your code here!
+document.querySelector("#main").remove()
+
+let newHeader = document.createElement("h1")
+
+var newContent = document.createTextNode("Dawn is the champion"); 
+
+newHeader.appendChild(newContent)
+
+newHeader.setAttribute("id", "victory");
+
+document.body.appendChild(newHeader);
+
